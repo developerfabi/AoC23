72a5a0500957d6fd217d45cf36152e2c7953d13c
Fabian Metzger <39843259+developerfabi@users.noreply.github.com>
2023-12-02 13:00:24 +0100

    Initial

---
+++ Day01/day_01.h
@@ -0,0 +1,12 @@
+//
+// Created by Fabian Metzger on 02.12.23.
+//
+
+#ifndef AOC23_DAY_01_H
+#define AOC23_DAY_01_H
+
+int day1_1(char input[]);
+int day1_2(char input[]);
+int searchForDigit(char text[]);
+
+#endif //AOC23_DAY_01_H
