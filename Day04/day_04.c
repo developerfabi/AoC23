0c19e95947f2fc163d02e12bc5155c1cb638f60f
Fabian Metzger <39843259+developerfabi@users.noreply.github.com>
2023-12-04 16:51:04 +0100

    Code day 4

--- Day04/day_04.c
+++ Day04/day_04.c
@@ -2,12 +2,122 @@
 // Created by Fabian Metzger on 04.12.23.
 //
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "../lib.h"
 #include "day_04.h"
 
+Card parseCard(char line[]) {
+    size_t inputLength = strlen(line);
+    int offset = 5;
+
+    Card card;
+    char * cardNumber = readUntilCharacter(line, ':', &offset, 10);
+    card.cardNumber = atoi(cardNumber);
+    card.numberOfWinningNumbers = 0;
+    card.nuberOfOwnNumbers = 0;
+    card.winningNumbers = NULL;
+    card.ownNumbers = NULL;
+
+    CharBuffer numberBuffer = createCharBuffer(10);
+
+    bool readingWinningNumbers = true;
+
+    offset += 1;
+    while (offset <= inputLength) {
+        if (offset >= inputLength || line[offset] == ' ') {
+            if (charBufferHasContent(&numberBuffer)) {
+                int number = atoi(numberBuffer.buffer);
+
+                if (readingWinningNumbers) {
+                    card.winningNumbers = realloc(card.winningNumbers, (card.numberOfWinningNumbers + 1) * sizeof(int));
+                    card.winningNumbers[card.numberOfWinningNumbers] = number;
+                    card.numberOfWinningNumbers += 1;
+                } else {
+                    card.ownNumbers = realloc(card.ownNumbers, (card.nuberOfOwnNumbers + 1) * sizeof(int));
+                    card.ownNumbers[card.nuberOfOwnNumbers] = number;
+                    card.nuberOfOwnNumbers += 1;
+                }
+
+                resetCharBuffer(&numberBuffer);
+            }
+        } else if (line[offset] == '|') {
+            readingWinningNumbers = false;
+        } else if (isdigit(line[offset])) {
+            writeToCharBuffer(line[offset], &numberBuffer);
+        }
+        offset ++;
+    }
+
+    return card;
+}
+
+int getCardMatches(Card card) {
+    int count = 0;
+    printf("Card %d: ", card.cardNumber);
+    for (int i=0; i < card.nuberOfOwnNumbers; i++) {
+        for (int j=0; j < card.numberOfWinningNumbers; j++) {
+            if (card.ownNumbers[i] == card.winningNumbers[j]) {
+                count += 1;
+                printf("%d, ", card.ownNumbers[i]);
+                j = card.numberOfWinningNumbers; // End loop
+            }
+        }
+    }
+    printf("\n");
+    return count;
+}
+
+int getCardPoints(Card card) {
+    int count = getCardMatches(card);
+    if (count > 0) return (1 << (count - 1)); // = 2^count
+    else return 0;
+}
+
 int day4_1(char input[]) {
-    return -1;
+    size_t inputLength = strlen(input);
+    int total = 0;
+
+    int offset = 0;
+    while (offset < inputLength) {
+        char * line = readUntilCharacter(input, '\n', &offset, 100);
+        Card card = parseCard(line);
+        free(line);
+
+        total += getCardPoints(card);
+    }
+
+    return total;
 }
 
 int day4_2(char input[]) {
-    return -1;
+    size_t inputLength = strlen(input);
+
+    int numberOfUniqueCards = 0;
+    CardWithCopies * uniqueCards = NULL;
+
+    int offset = 0;
+    while (offset < inputLength) {
+        char * line = readUntilCharacter(input, '\n', &offset, 100);
+        Card card = parseCard(line);
+        free(line);
+
+        uniqueCards = realloc(uniqueCards, (numberOfUniqueCards + 1) * sizeof(CardWithCopies));
+        uniqueCards[numberOfUniqueCards].card = card;
+        uniqueCards[numberOfUniqueCards].count = 1;
+        numberOfUniqueCards += 1;
+    }
+
+    int total = 0;
+    for (int i=0; i < numberOfUniqueCards; i++) {
+        int matches = getCardMatches(uniqueCards[i].card);
+        for (int j=i+1; j<=i+matches && j<numberOfUniqueCards; j++) {
+            printf("Adding %d copies to card %d\n", uniqueCards[i].count, uniqueCards[j].card.cardNumber);
+            uniqueCards[j].count += uniqueCards[i].count;
+        }
+        total += uniqueCards[i].count;
+    }
+    return total;
 }
--- Day04/day_04.h
+++ Day04/day_04.h
@@ -5,6 +5,23 @@
 #ifndef AOC23_DAY_04_H
 #define AOC23_DAY_04_H
 
+typedef struct {
+    int cardNumber;
+    int numberOfWinningNumbers;
+    int * winningNumbers;
+    int nuberOfOwnNumbers;
+    int * ownNumbers;
+} Card;
+
+typedef struct {
+    Card card;
+    int count;
+} CardWithCopies;
+
+Card parseCard(char line[]);
+int getCardMatches(Card card);
+int getCardPoints(Card card);
+
 int day4_1(char input[]);
 int day4_2(char input[]);
 
