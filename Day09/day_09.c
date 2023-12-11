fd6b3900e9f64012549c5561d05809ec23d7562d
Fabian Metzger <39843259+developerfabi@users.noreply.github.com>
2023-12-11 20:16:37 +0100

    Code day 9

--- Day09/day_09.c
+++ Day09/day_09.c
@@ -2,12 +2,73 @@
 // Created by Fabian Metzger on 04.12.23.
 //
 
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "day_09.h"
+#include "../lib.h"
 
-int day9_1(char input[]) {
-    return -1;
+int64_t extrapolate(int64_t * sequence, int numberOfSequenceItems, bool backwards) {
+    int64_t ** layers = malloc(sizeof(int64_t *));
+    layers[0] = sequence;
+    int numberOfLayers = 1;
+    while (!allZero(layers[numberOfLayers - 1], numberOfSequenceItems - numberOfLayers + 1)) {
+        layers = realloc(layers, (numberOfLayers+1) * sizeof(int64_t *));
+        layers[numberOfLayers] = calloc(numberOfSequenceItems-numberOfLayers, sizeof(int64_t));
+        for (int i=0; i<numberOfSequenceItems-numberOfLayers; i++) {
+            layers[numberOfLayers][i] = layers[numberOfLayers-1][i+1] - layers[numberOfLayers-1][i];
+        }
+        numberOfLayers += 1;
+    }
+
+    int64_t total = 0;
+    bool positive = true;
+    for (int i=0; i<(numberOfLayers-1); i++) {
+        int64_t value = layers[i][backwards ? 0 : numberOfSequenceItems-i-1];
+        printf("%lld +/- ", value);
+        if (backwards && !positive) {
+            total -= value;
+            positive = !positive;
+        } else {
+            total += value;
+            positive = !positive;
+        }
+    }
+    printf("0 = %lld\n", total);
+
+    free(layers);
+    return total;
 }
 
-int day9_2(char input[]) {
-    return -1;
+int64_t day9_1(char input[]) { // 820847869
+    size_t inputLength = strlen(input);
+
+    int64_t total = 0;
+
+    int offset = 0;
+    while (offset < inputLength) {
+        char * line = readUntilCharacter(input, '\n', &offset, 100);
+        int numberOfSequenceItems;
+        int64_t * sequence = readNumbers(line, &numberOfSequenceItems);
+        total += extrapolate(sequence, numberOfSequenceItems, false);
+    }
+
+    return total;
+}
+
+int64_t day9_2(char input[]) {
+    size_t inputLength = strlen(input);
+
+    int64_t total = 0;
+
+    int offset = 0;
+    while (offset < inputLength) {
+        char * line = readUntilCharacter(input, '\n', &offset, 100);
+        int numberOfSequenceItems;
+        int64_t * sequence = readNumbers(line, &numberOfSequenceItems);
+        total += extrapolate(sequence, numberOfSequenceItems, true);
+    }
+
+    return total;
 }
--- Day09/day_09.h
+++ Day09/day_09.h
@@ -5,7 +5,11 @@
 #ifndef AOC23_DAY_09_H
 #define AOC23_DAY_09_H
 
-int day9_1(char input[]);
-int day9_2(char input[]);
+#include <stdbool.h>
+
+int64_t extrapolate(int64_t * sequence, int numberOfSequenceItems, bool backwards);
+
+int64_t day9_1(char input[]);
+int64_t day9_2(char input[]);
 
 #endif //AOC23_DAY_09_H
